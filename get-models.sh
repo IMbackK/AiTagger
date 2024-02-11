b49f35cf98ee2a93779d6d49cbb14dff1e8eba61
uvos <devnull@uvos.xyz>
2024-02-11 15:18:57 +0100

    update get-models script

---
+++ get-models.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+mkdir net
+wget https://uvos.xyz/projects/aitagger/tags-gpt2-medium.gguf -O net/tags-gpt2-medium.gguf
+wget https://uvos.xyz/projects/aitagger/tags-gpt2-medium-v2.gguf -O net/tags-gpt2-medium-v2.gguf
